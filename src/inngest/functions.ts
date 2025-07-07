6ff1de9545438d8a41102add6db77d63ad4815bf
Riyan <griyan115@gmail.com>
2025-07-07 23:35:47 +0530

    06-e2b-sandboxes

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,20 @@
 import { openai, createAgent } from "@inngest/agent-kit";
 import { inngest } from "./client";
+import { Sandbox } from "@e2b/code-interpreter";
+import { getSandbox } from "./utils";
+import "dotenv/config";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
   async ({ event, step }) => {
+    const sandboxId = await step.run("get-sandbox-id", async () => {
+      const sandbox = await Sandbox.create("forge-ai-test-3", {
+        apiKey: process.env.E2B_API_KEY!,
+      });
+      return sandbox.sandboxId;
+    });
+
     const codeAgent = createAgent({
       name: "code-agent",
       system:
@@ -15,8 +25,13 @@ export const helloWorld = inngest.createFunction(
     const { output } = await codeAgent.run(
       `write the following snippet: ${event.data.value}`
     );
-    console.log(output);
 
-    return { output };
+    const sandboxUrl = await step.run("get-sandbox-url", async () => {
+      const sandbox = await getSandbox(sandboxId);
+      const host = sandbox.getHost(3000);
+      return `https://${host}`;
+    });
+
+    return { output, sandboxUrl };
   }
 );
